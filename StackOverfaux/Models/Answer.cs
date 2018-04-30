21259042bc72d14253d7eb9fa16ff98b11df378f
Kayla Ondracek and Jasun Feddema <kayla.renee@gmail.com and jasun.feddema@gmail.com>
2018-04-30 15:26:03 -0700

    add question and answer classes

---
+++ StackOverfaux/Models/Answer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverfaux.Models
+{
+    [Table("Answers")]
+    public class Answer
+    {
+        [Key]
+        public int AnswerId { get; set; }
+        public string TextBody { get; set; }
+        public bool AcceptedAnswer { get; set; }
+        public int Votes { get; set; }
+        public DateTime Date { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public virtual Question Question { get; set; }
+
+    }
+}
---
+++ StackOverfaux/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverfaux.Models
+{
+    [Table("Questions")]
+    public class Question
+    {
+        [Key]
+        public int QuestionId { get; set; }
+        public string Title { get; set; }
+        public string BodyText { get; set; }
+        public int Votes { get; set; }
+        public DateTime Date { get; set; }
+        public virtual List<Answer> Answers { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+    }
+}
